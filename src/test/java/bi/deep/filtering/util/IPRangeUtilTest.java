eb4a1b30b771e85a03e1d11c159dec8e42fcf98a
Misha <mikhailsviatohorof@gmail.com>
2025-08-20 13:19:19 +0200

    CIDR and single ip support added to ip_multi_range (#10)
    * CIDR and single ip support added to ip_multi_range
    
    * Parametrized tests added
    
    * Merged ipRange parsing into IPRangeUtil.fromString()
    
    * Removed Pair from IPRangeUtil
    
    (cherry picked from commit 1cfeffb1ab49ebe8ded72ae6c7d7ea322b8a3534)

--- src/test/java/bi/deep/filtering/util/IPRangeUtilTest.java
+++ src/test/java/bi/deep/filtering/util/IPRangeUtilTest.java
@@ -1,25 +1,23 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright Deep BI, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package bi.deep.filtering.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import bi.deep.entity.IPSetContents;
@@ -32,16 +30,9 @@ import inet.ipaddr.IPAddressString;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-import org.apache.druid.common.config.NullHandling;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class IPRangeUtilTest {
-
-    @BeforeAll
-    static void setup() {
-        NullHandling.initializeForTests();
-    }
 
     @Test
     void testExtractIPRanges() {
@@ -54,7 +45,7 @@ class IPRangeUtilTest {
     void testGetMatchingIP_EmptyResult() {
         String input = "192.168.1.1";
         List<IPAddress> ips = IPRangeUtil.mapStringsToIps(Sets.newHashSet("10.0.0.1"));
-        assertEquals(NullHandling.sqlCompatible() ? null : "", IPRangeUtil.getMatchingIPs(input, ips));
+        assertNull(IPRangeUtil.getMatchingIPs(input, ips));
     }
 
     @Test
@@ -76,7 +67,7 @@ class IPRangeUtilTest {
     void testGetMatchingIPRanges_EmptyResult() {
         String input = "192.168.1.1-192.168.1.100";
         List<IPAddress> ips = IPRangeUtil.mapStringsToIps(Sets.newHashSet("10.0.0.1"));
-        assertEquals(NullHandling.sqlCompatible() ? null : "", IPRangeUtil.getMatchingIPs(input, ips));
+        assertNull(IPRangeUtil.getMatchingIPs(input, ips));
     }
 
     @Test
